26a86220a5ad7837fdd1a759aa8bbdafdd1bd299
Irakli <99668662+Irakli66@users.noreply.github.com>
2024-12-21 18:22:24 +0400

    add: starter app

---
+++ Wazap/Wazap/WazapApp.swift
@@ -0,0 +1,27 @@
+//
+//  WazapApp.swift
+//  Wazap
+//
+//  Created by irakli kharshiladze on 21.12.24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+@main
+struct WazapApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
